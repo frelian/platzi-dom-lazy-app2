024ec73a791608c9b792377db8bdb55e4e652dfa
frelian <frelian@users.noreply.github.com>
2022-02-18 21:05:48 +0000

    deploy: f8458f227651cfc8ef9545d28f7fd5a995c68fc0

---
+++ _dist_/index-intersectionOb-fecth.js
@@ -0,0 +1,51 @@
+/*
+    Este codigo es el mismo que index.js solo que no funciono obtener la imagen en loadImage (lazy.js)
+    en el entry.target el innterHTML no contenia la imagen
+*/
+
+import { registerImage } from './lazy.js';
+
+const maximum = 122;
+const minimum = 1;
+const random = () => Math.floor(Math.random() * (maximum - minimum) + minimum);
+
+const baseUrl = "https://randomfox.ca/floof/";
+
+const createImageNode = () => {
+
+    // Creo contenedor
+    const container = document.createElement('div');
+
+    // Haciendo el random manualmente: imagen.src = `https://randomfox.ca/images/${random()}.jpg`;
+    // o uso fetch
+    window.fetch( baseUrl )
+        .then( resp => resp.json() )
+        .then( respJson => {
+
+            container.className = 'p-4';
+
+            // Creo imagen
+            const img = document.createElement('img');
+            img.className = 'mx-auto';
+            img.width = '300';
+            img.src = respJson.image;
+
+            // Agrego la imagen al contenedor
+            container.appendChild( img );
+        })
+
+    return container;
+}
+
+const nuevaImagen = createImageNode();
+const mountNode = document.getElementById("images");
+const button = document.querySelector('button');
+
+// Agrego un listener si al hacer clic reacciono a él.
+const addImage = () => {
+    const newImage = createImageNode();
+    mountNode.appendChild(newImage);
+    registerImage(newImage)
+};
+
+button.addEventListener("click", addImage);
--- _dist_/lazy.js
+++ _dist_/lazy.js
@@ -3,20 +3,31 @@ const isIntersecting = (entry) => {
     return entry.isIntersecting // true si esta dentro de la pantalla
 }
 
-const action = (entry) => {
-    const nodo = entry.target;
+const loadImage = (entry) => {
+    const container = entry.target; // container (DIV)
 
-    console.log("holis");
+    // Como se que la imagen es el 1er elemento del DIV entonces uso firstChild ya no es necesario: container.querySelector("img")
+    const image = container.firstChild;
+    const url = image.dataset.src;
+
+    // Cargo la imagen
+    image.src = url;
+    
+    // debugger;
+    // console.log(container.nodeName);
+
+    loadedImages++;
+    printLog();
 
     // Des-registra o deja descuchar la imagen
-    observer.unobserve(nodo);
+    observer.unobserve(container);
 }
 
 const observer = new IntersectionObserver((entries) => {
-    entries.filter(isIntersecting).forEach(action);
+    entries.filter(isIntersecting).forEach(loadImage);
 })
 
-export const registerImagen = (image) => {
+export const registerImage = (image) => {
     // Le dire a Intersectation Observer que observe la imagen 
     observer.observe(image);
 }
