f8458f227651cfc8ef9545d28f7fd5a995c68fc0
Julian N <ingsistemasjulian@gmail.com>
2022-02-18 16:05:10 -0500

    Terminado lazy-loading agregado limpiador y registro de contadores en consola

--- src/index.js
+++ src/index.js
@@ -1,47 +1,73 @@
-import { registerImagen } from './lazy';
+import { registerImage } from './lazy';
 
-
-const maximum = 122;
-const minimum = 1;
-const random = () => Math.floor(Math.random() * (maximum - minimum) + minimum);
-
-const baseUrl = "https://randomfox.ca/floof/";
+const maximun = 121;
+const minimun = 1;
+const random = () => Math.floor(Math.random()*(maximun - minimun)+ minimun)
 
 const createImageNode = () => {
-
-    // Creo contenedor
     const container = document.createElement('div');
+    container.className = "p-4";
 
-    // Haciendo el random manualmente: imagen.src = `https://randomfox.ca/images/${random()}.jpg`;
-    // o uso fetch
-    window.fetch( baseUrl )
-        .then( resp => resp.json() )
-        .then( respJson => {
+    const imagen = document.createElement('img');
+    imagen.className = "mx-auto";
+    imagen.width = '320';
 
-            container.className = 'p-4';
+    /*
+      1. forma
+      Si no queremos que una imagen cargue tenemos que eliminar
+      la parte que la carga ya que esta linea trae la url de la imagen
+      por lo tanto la hara cargar siempre.
 
-            // Creo imagen
-            const img = document.createElement('img');
-            img.className = 'mx-auto';
-            img.width = '300';
-            img.src = respJson.image;
+      Pasaremos este trabajo de cargar la imagen al lazy loading
 
-            // Agrego la imagen al contenedor
-            container.appendChild( img );
-        })
+      2.forma
 
-    return container;
+      La informacion de la url se la dejaremos a la imagen pero 
+      utilizaremos el dataset  que es una propiedad de html
+
+      Dataset se utiliza mucho para comunicar informacion entre html
+      y js, es muy comun tener diferentes data-? para comunicar la info
+    
+    */
+
+    //imagen.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+    // Esto agregara a la propieda data-src de mi imagen la url
+    imagen.dataset.src = `https://randomfox.ca/images/${random()}.jpg`;
+    
+  
+    container.appendChild(imagen)
+
+    appendedImages++;
+    printLog();
+
+    return container
 }
 
-const nuevaImagen = createImageNode();
-const mountNode = document.getElementById("images");
-const button = document.querySelector('button');
+const mountNode = document.getElementById('images');
+const addButton = document.querySelector('button');
+const cleanButton = document.getElementById('clean');
 
-// Agrego un listener si al hacer clic reacciono a él.
-const addImage = () => {
+const addImage = () => 
+{ 
     const newImage = createImageNode();
-    mountNode.appendChild(newImage);
-    registerImagen(newImage);
-};
+    mountNode.append(newImage);
+    registerImage(newImage);
+}
 
-button.addEventListener("click", addImage);
+const cleanImages = () => {
+
+    console.log(mountNode.childNodes);
+
+    [...mountNode.childNodes].forEach(child => {
+        child.remove();
+    }) 
+
+    // limpio contadores
+    appendedImages = 0;
+    loadedImages = 0;
+
+}
+
+addButton.addEventListener("click", addImage)
+cleanButton.addEventListener("click", cleanImages)
